f03e6e35127d85a172cb63456e6c06ba3b0c01e2
mia <mia@miaui-MacBookAir.local>
2021-12-22 23:10:31 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: 'babel-loader',
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template/index.html',
+    })
+  ],
+  mode: 'production'
+};
